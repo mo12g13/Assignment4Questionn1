4dec3162542199049c18ecf4bd128d915ee71ee1
Momo Johnson <mo12g13@gmail.com>
2016-03-06 16:09:21 -0600

    Lakes

--- src/com/Momo/Main.java
+++ src/com/Momo/Main.java
@@ -1,79 +1,77 @@
 package com.Momo;
-
-import java.util.ArrayList;
-import java.util.DoubleSummaryStatistics;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        ArrayList<Double> runtime = new ArrayList<>();
+        HashMap<String, ArrayList<Double>> map = new HashMap<>();
+        double runnningTime =0;
+       String lake = null  ;
 
-        ArrayList<Double> cedarrunTime = new ArrayList<>();
-        ArrayList<Double> harrietrunTime = new ArrayList<>();
-        ArrayList<Double> comoRunTime = new ArrayList<>();
-        ArrayList<Double> otherRunTime = new ArrayList<>();
-
-
-        String lake;
-        String time;
-
-
-
-        boolean bool = true;
-        Double min = 89.0;
-        String othersLakes =null;
+        boolean istrue = true;
 
         do {
-            System.out.println("Enter the lake name");
-            lake = input.nextLine();
+            try {
+                System.out.println("what lake did you run around today? ");
+                 lake = input.nextLine();
 
-            System.out.println("How many minutes did you run: ");
-            time = input.nextLine();
-            if (lake.equals("") || time.equals(""))
-                break;
-            double runTime = Double.parseDouble(time);
+                System.out.println("How many minutes did you run");
+                String run =input.nextLine();
+                if(lake.equalsIgnoreCase("") && run.equalsIgnoreCase("")){
+                    break;
+                }
+                 runnningTime = Double.parseDouble(run);
+                istrue = true;
 
-            if (lake.equalsIgnoreCase("Cedar")) {
-                cedarrunTime.add(runTime);
 
-            } else if (lake.equalsIgnoreCase("Harriet")) {
-                harrietrunTime.add(runTime);
-
-            } else if (lake.equalsIgnoreCase("Como")) {
-                comoRunTime.add(runTime);
-            } else {
-                otherRunTime.add(runTime);
-                othersLakes = lake;
+            }catch (InputMismatchException e){
+                input.nextLine();
+                System.out.println("Please enter the right number");
+                istrue = false;
 
             }
+            map.put(lake, runtime);
+            if (map.containsKey(lake)) {//adding the time if the lake is in the haspmap
+                runtime.add(runnningTime);
+            } else {
+                runtime.add(runnningTime);//adding lake if the lake isn't in the hashmap
+                map.put(lake, runtime);
+            }
+        }while (true);
+        ArrayList<Double> name = new ArrayList<>();
+        String valuekey =null;
+
+        for(Map.Entry<String, ArrayList<Double>> key: map.entrySet()){
+             valuekey = key.getKey();
+            name = key.getValue();
+            System.out.println(valuekey + " "+ name);
+            double finalFasted = fastedRun(name);//getting the fasted value from the arraylist
+            System.out.println(valuekey + " "+ finalFasted);
 
 
-        } while (true);
+        }
 
 
-        double harriet = fastedRun(lake,harrietrunTime);
-        double cedar = fastedRun(lake, cedarrunTime);
-        double como = fastedRun(lake, comoRunTime);
-        double others = fastedRun(lake, otherRunTime);
+       /* for (String key : map.keySet()) {
+            System.out.println(key + " " + map.get(key));
+        }*/
 
-        System.out.println("Harriet fasted run: "+ harriet);
-        System.out.println("Como fasted run: "+ como);
-        System.out.println("Cedar fasted run: "+ cedar);
-        System.out.println(othersLakes + " fasted run: "+ others);
+     }
 
 
-    }
 
-    public static double fastedRun (String lake, ArrayList<Double> NewArray){
-        Double HarrietMin = 90.00;
+
+    public static double fastedRun (ArrayList<Double> NewArray){
+        Double min = 90.00;//A method that return the fasted time
         Double fastRun=0.0;
 
         for(int i =0; i<NewArray.size(); i++){
-            if(NewArray.get(i)< HarrietMin){
-                HarrietMin= NewArray.get(i);
-                fastRun = HarrietMin;
+            if(NewArray.get(i)< min){
+                min= NewArray.get(i);
+                fastRun = min;
             }
         }
 
